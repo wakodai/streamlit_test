10c8b89eaa15b0d708238006ad2313fdb8c149c3
wako_daisuke <wako_daisuke@kurusugawa.jp>
2023-01-25 12:21:45 +0900

    weather

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
-x = 10
-'x: ', x
+# x = 10
+# 'x: ', x
+import pandas as pd
+# from selenium.webdriver.support.ui import Select
+# from selenium import webdriver
+import streamlit as st
+
+df = pd.read_html("https://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=51&block_no=47636&year=2023&month=1&day=23&view=", index_col=0)
+df = df[0]
+st.write(df)
